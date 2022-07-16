6e1e5f4e3127435f513258692d4079e13d98b13b
ananya-bangera <84728713+ananya-bangera@users.noreply.github.com>
2022-07-17 02:41:56 +0530

    Updated Timer

--- android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/ml/PoseClassifier.kt
+++ android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/ml/PoseClassifier.kt
@@ -65,6 +65,7 @@ class PoseClassifier(
 
     fun classify(person: Person?): List<Pair<String, Float>> {
         // Preprocess the pose estimation result to a flat array
+
         val inputVector = FloatArray(input[1])
         person?.keyPoints?.forEachIndexed { index, keyPoint ->
             inputVector[index * 3] = keyPoint.coordinate.y
@@ -79,15 +80,17 @@ class PoseClassifier(
         outputTensor.forEachIndexed { index, score ->
             output.add(Pair(labels[index], score))
         }
+
         if(firebaseOutput.size==0){
             for(ele in output){
                 firebaseOutput.add(ele)
             }
         }
         else{
+
             var cnt = 0
             for(ele in output){
-                if(ele.second>firebaseOutput[cnt].second){
+                if(ele.second>=0.97){
                     firebaseOutput.set(cnt,ele)
                 }
                 cnt = cnt+1
@@ -99,9 +102,19 @@ class PoseClassifier(
     }
 
     fun close() {
+        val finalDateTime: Date = Calendar.getInstance().time
+        val finalDateTimeAsLong: Long = finalDateTime.time
+        var diff = (finalDateTimeAsLong-dateTimeAsLong)/1000
         val myMap = mutableMapOf<String, String>()
+        println("%%%%%%%%%%%%%%%%%%%%%${dateTimeAsLong}%%%%%%%%%${finalDateTimeAsLong}%%%%%%%%%%%%%%5")
         for(ele in firebaseOutput){
-            myMap[ele.first.toString()] = ele.second.toString()
+            if(ele.second>=0.5){
+                myMap[ele.first.toString()] = diff.toString()
+            }
+            else{
+                myMap[ele.first.toString()] = "0.0"
+            }
+
         }
         println(myMap)
         db.collection("workout").document(userEmail.toString()).collection(dateTimeAsLong.toString()).document("poses").set(myMap)
