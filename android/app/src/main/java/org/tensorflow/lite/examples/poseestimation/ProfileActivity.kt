932fee94fac1d17dc9ab68fb8ce44763ca82d4ec
Harsh <harshnag23@gmail.com>
2022-07-17 15:36:27 +0530

    Minute changes

--- android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/ProfileActivity.kt
+++ android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/ProfileActivity.kt
@@ -4,21 +4,38 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.FirebaseApp
 import org.tensorflow.lite.examples.poseestimation.databinding.ActivityProfileBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Query
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
     private lateinit var actionBar: ActionBar
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+        database = Firebase.database.reference
+
+        database=FirebaseDatabase.getInstance().getReference("workout")
+        database.get().addOnCompleteListener {
+
+        }
+
         checkUser()
         binding.logoutBtn.setOnClickListener{
             firebaseAuth.signOut()
